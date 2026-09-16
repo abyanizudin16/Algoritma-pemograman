04453e8e61614695066a97966b2e6150e0060883
abyanizudin16 <abyanizudin55@gmail.com>
2026-09-16 14:49:58 +0700

    Add files via upload

---
+++ latihan.py1.py
@@ -0,0 +1,16 @@
+# ==========================================
+# PROGRAM PENGENALAN VARIABEL PYTHON
+# ==========================================
+
+# Membuat variabel
+nama = "M.Abyan"
+umur = 18
+tinggi = 167.5
+belum_menikah = False
+
+# Menampilkan nilai dan tipe variabel
+print("Nama         :", nama, "| Tipe :", type(nama))
+print("Umur         :", umur, "| Tipe :", type(umur))
+print("Tinggi       :", tinggi, "| Tipe :", type(tinggi))
+print("Belum Menikah:", belum_menikah, "| Tipe :", type(belum_menikah))
+
